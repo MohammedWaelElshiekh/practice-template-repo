532a7c43cd78dbdf4ea9745c9a691269ed699db0
Mohammed Wael Elshiekh <mohammedwaelelshiekhmw@gmail.com>
2024-03-15 17:08:44 +0200

    Update file.js

--- file.js
+++ file.js
@@ -5,31 +5,32 @@ function movePlayer(direction){
                 (resolve,reject) =>{
                     setTimeout(resolve,1500, "the player has moved forward")
                 }
-            )
+            ).then(message => {console.log(message)})
             
         case "backward":
             return new Promise(
                 (resolve,reject) =>{
                     setTimeout(resolve,1500, "the player has moved backward")
                 }
-            )
+            ).then(message => {console.log(message)})
             
         case "right":
             return new Promise(
                 (resolve,reject) =>{
                     setTimeout(resolve,1500, "the player has moved right")
                 }
-            )
+            ).then(message => {console.log(message)})
             
         case "left":
             return new Promise(
                 (resolve,reject) =>{
                     setTimeout(resolve,1500, "the player has moved left")
                 }
-            )
+            ).then(message => {console.log(message)})
             
     
         default:
             break;
     }
 }
+
